0cd5c31d12dc6e977173a0a9e8ba998bca5561ea
klawir <klawir1@o2.pl>
2019-12-16 21:11:58 +0100

    Implemented player's movement

---
+++ Assets/Data/Player/control/Keyboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour
+{
+    public GameObject playerTransf;
+    public GameObject playerModel;
+    public CharacterController characterController;
+
+    private Vector3 rotate;
+
+    private Command movement;
+
+    private void Start()
+    {
+        movement = new Movement(playerTransf);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            movement.Execute();
+    }
+}
---
+++ Assets/Data/Player/control/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour, Command
+{
+    private Vector3 pos;
+    private CharacterController characterController;
+    private Player player;
+    
+    public Movement(GameObject playerObj)
+    {
+        player = playerObj.GetComponent<Player>();
+        characterController = playerObj.GetComponent<CharacterController>();
+    }
+    public void Execute()
+    {
+        CalculateDirection();
+        characterController.Move(pos * player.speed * Time.deltaTime);
+    }
+
+    private void CalculateDirection()
+    {
+        pos = player.transform.right * Input.GetAxis("Horizontal") + player.transform.forward * Input.GetAxis("Vertical");
+    }
+}
